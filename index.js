e97d967c1e5080798ce81110e3c029e7b70ebf71
sho.yamashita <sho.yamashita@japan-reit.co.jp>
2019-02-27 21:26:34 +0900

    +1 1度のpushに複数のコミットが含まれている場合、すべてのコミットが対象となるように修正

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
+//課題番号の抽出条件
+const ISSUE_PATTERN = /.+-[0-9]+/;
+
 // TODO:もろもろエラーチェック。commitがないときとか。
-module.exports = async function (context, req) {
-    var request = require('request');
-
-    var conf = require('./conf');
-
-    //request bodyの書き出し（確認用）
-    context.log(req.body);
-
-    //コミットコメントから課題番号を抽出
-    //FIXME:１コミットに複数の課題IDが含まれている場合は未考慮
-    var issuePattern = /.+-[0-9]+/;
-    var commit = req.body.commits[0];
-    context.log(commit.message);
-    var issueId = commit.message.match(issuePattern);
-    context.log(issueId);
+function postIssueComment(request,conf,context, branchName, commit) {
+    //feat: 等は除く
+    //context.log('メッセージ is'+commit.message);
+    var issueId = commit.message.replace(/.+: /, '').match(ISSUE_PATTERN);
     if(!issueId){
-        context.res = {
-            body:"Issue ID was not included."
-        };
-        context.done();
+        return;
     }
-
-    var branchName = (req.body.ref).replace('refs/heads/','');
+    context.log('issueId is ' + issueId);
 
     //課題書き込み用のコメントを生成
-    var issueComment = `[${commit.committer.name} が [${branchName}] ブランチにコミット
+    var issueComment = `${commit.committer.name} が [${branchName}] ブランチにコミット
 ${commit.url}
             
 <<コミットコメント>>
@@ -33,7 +21,7 @@ ${commit.message}`;
     //課題コメント用のリクエスト情報を生成する。
     //https://developer.nulab-inc.com/ja/docs/backlog/api/2/add-comment/
     var options = {
-            headers: 'Content-Type:application/x-www-form-urlencoded',
+            headers: 'Content-Type:application/x-www-form-urlencoded ',
             method: 'POST',
             url: `https://${conf.spaceId}.backlog.jp/api/v2/issues/${issueId}/comments?apiKey=${conf.apiKey}`,
             form: {"content": issueComment},
@@ -46,6 +34,23 @@ ${commit.message}`;
             body:issueId + " commented."
         };
     })
+}
+
+module.exports = async function (context, req) {
+    var request = require('request');
+
+    var conf = require('./conf');
+
+    //request bodyの書き出し（確認用）
+    //context.log(req.body);
+
+    //ブランチ名の取得
+    var branchName = (req.body.ref).replace('refs/heads/','');
+
+    for(var index in req.body.commits){
+        //バックログにPOST
+        postIssueComment(request,conf,context,branchName,req.body.commits[index]);
+    }
     context.log("function finish");
     context.done();
 };
